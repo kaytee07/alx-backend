54c33f4c9e093bdccc1770392467ff12323758a1
kaytee07 <kofi.taylor07@gmail.com>
2023-10-24 14:44:13 +0000

    fifocache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+FIFOCache module
+"""
+from basic_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Defines FIFOCache
+    - where our data will be stored
+    - how to retrieve data from the cache
+    """
+    def __init__(self):
+        """
+        inherit Parent class BaseCaching
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        store data into cache
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            new_key, new_value = self.cache_data.popitem()
+            print(f"DISCARD: {new_key}")
+
+    def get(self, key):
+        """
+        return value linked to key in cache_data
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
