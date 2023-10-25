22263723a7f0c226027d6125cbf6b9c36a660009
kaytee07 <kofi.taylor07@gmail.com>
2023-10-25 06:48:03 +0000

    Implementing the fifo algorithm

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,6 +15,7 @@ class FIFOCache(BaseCaching):
         """
         inherit Parent class BaseCaching
         """
+        self.insertion_order = []
         super().__init__()
 
     def put(self, key, item):
@@ -23,12 +24,13 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-
+        self.insertion_order.append(key)
         self.cache_data[key] = item
-
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            new_key, new_value = self.cache_data.popitem()
+            new_key = self.insertion_order[0]
+            del self.cache_data[new_key]
             print(f"DISCARD: {new_key}")
+            self.insertion_order.pop(0)
 
     def get(self, key):
         """
