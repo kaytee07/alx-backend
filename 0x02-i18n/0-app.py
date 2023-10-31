c0063f54c6205f1b6ea310a40b429c863cf65988
kaytee07 <kofi.taylor07@gmail.com>
2023-10-31 17:43:48 +0000

    add

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,12 +7,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def get_index() -> str:
     """
     return html index html template
     """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
