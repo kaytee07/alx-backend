379e25d7318f2e3441ceb50b52227a39ba330748
kaytee07 <kofi.taylor07@gmail.com>
2023-11-29 17:14:40 +0000

    processing

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import { createQueue } from 'kue';
+
+
+const queue = createQueue();
+
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: "+233201775908",
+  message: "debit made successfully",
+});
+
+job
+    .on('enqueue', () => {
+	console.log('Notification job created:', job.id);
+    })
+    .on('complete', () => {
+	console.log('Notification job completed');
+    })
+    .on('failed attempt', () => {
+	console.log('Notification job failed');
+    });
+
+job.save();
